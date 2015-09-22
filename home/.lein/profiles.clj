f710635d3f33e988b32593c3c067e77740676bd3
Adrien Thebo <git@somethingsinistral.net>
2015-09-22 10:52:25 -0700

    Add leiningen user profiles

---
+++ home/.lein/profiles.clj
@@ -0,0 +1,14 @@
+{:user {:plugins [[lein-pprint "1.1.2"]
+                  [lein-voom "0.1.0-20140716_032004-g85e4c9b" :exclusions [org.clojure/clojure]]]
+                  ;[venantius/ultra "0.3.4" :exclusions [org.clojure/clojure]]]
+
+        :ultra {:color-scheme :solarized_dark}
+
+        :dependencies [[rubydoc "0.4.0"]
+                       [clj-stacktrace "0.2.8"]
+                       [spyscope "0.1.5"]]
+
+        :injections [(require 'spyscope.core)
+                     (let [orig (ns-resolve (doto 'clojure.stacktrace require) 'print-cause-trace)
+                           new (ns-resolve (doto 'clj-stacktrace.repl require) 'pst)]
+                       (alter-var-root orig (constantly (deref new))))]}}
